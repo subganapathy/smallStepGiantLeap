fd7254b881eae56b120dd9c7022a57f9a002eb8d
U-spikeUCI-PC\spikeUCI <spikeUCI@spikeUCI-PC.(none)>
2012-06-20 15:19:34 -0700

    My initial commit of something related to Web :D

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SmallStepGiantLeap::Application.config.secret_token = '21a372f3833193f05eb1925ff7e1021ce403fc88185b0f99492dec129515c68ac5eb4e63406f92a9f6faf326f4bfd0ce36f8292af19a05d0e13da07d6befaf0a'
